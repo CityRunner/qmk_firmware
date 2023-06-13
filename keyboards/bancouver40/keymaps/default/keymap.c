5fb90139740a4424efc38737fa3886a0bcb36b59
CityRunner <bigcityrunner@gmail.comc>
2023-06-13 16:25:00 +0300

    Numeric and function layers

--- keyboards/bancouver40/keymaps/default/keymap.c
+++ keyboards/bancouver40/keymaps/default/keymap.c
@@ -56,56 +56,57 @@ enum bancouver40_layers {
 #define SYM_ESC LT(_SYMBOLIC,   KC_ESCAPE)
 #define FUN_ENT LT(_FUNCTION,   KC_ENTER)
 #define NAV_BSP LT(_NAVIGATION, KC_BSPACE)
+#define SYS_NUL MO(_SYSTEM)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT_ortho_4x10(
-    KC_Q,  KC_W,  KC_F,   KC_P,    KC_B,    KC_J,    KC_L,    KC_U,      KC_Y,    KC_SCLN,
-    GUI_A, ALT_R, SFT_S,  CTL_T,   KC_G,    KC_M,    CTL_N,   SFT_E,     ALT_I,   GUI_O,
-    KC_Z,  KC_X,  KC_C,   KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM,   KC_DOT,  KC_SLSH,
-    KC_NO, KC_NO, KC_TAB, NUM_SPC, SYM_ESC, FUN_ENT, NAV_BSP, KC_DELETE, KC_VOLD, KC_VOLU
+    KC_Q,    KC_W,   KC_F,   KC_P,    KC_B,    KC_J,    KC_L,    KC_U,      KC_Y,    KC_SCLN,
+    GUI_A,   ALT_R,  SFT_S,  CTL_T,   KC_G,    KC_M,    CTL_N,   SFT_E,     ALT_I,   GUI_O,
+    KC_Z,    KC_X,   KC_C,   KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM,   KC_DOT,  KC_SLSH,
+    SYS_NUL, KC_APP, KC_TAB, NUM_SPC, SYM_ESC, FUN_ENT, NAV_BSP, KC_DELETE, KC_VOLD, KC_VOLU
     ),
 
   [_QWERTY] = LAYOUT_ortho_4x10(
-    KC_Q,    KC_W,  KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,
-    GUI_A,   ALT_S, SFT_D,  CTL_F,   KC_G,    KC_H,    CTL_J,   SFT_K,     ALT_L,   GUI_SCLN,
-    KC_Z,    KC_X,  KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH,
-    KC_LGUI, KC_NO, KC_TAB, NUM_SPC, SYM_ESC, FUN_ENT, NAV_BSP, KC_DELETE, KC_VOLD, KC_VOLU
+    KC_Q,    KC_W,   KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,
+    GUI_A,   ALT_S,  SFT_D,  CTL_F,   KC_G,    KC_H,    CTL_J,   SFT_K,     ALT_L,   GUI_SCLN,
+    KC_Z,    KC_X,   KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH,
+    SYS_NUL, KC_APP, KC_TAB, NUM_SPC, SYM_ESC, FUN_ENT, NAV_BSP, KC_DELETE, KC_VOLD, KC_VOLU
     ),
 
   [_NUMERIC] = LAYOUT_ortho_4x10(
-    KC_1,      KC_2,     KC_3,     KC_4,        KC_5,        KC_6,    KC_7,    KC_8,      KC_9,     KC_0,
-    KC_GRAVE,  KC_MINUS, KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_LEFT, KC_DOWN, KC_UP,     KC_RIGHT, KC_QUOTE,
-    KC_BSLASH, KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_QUOTE,
-    KC_NO,     KC_NO,    KC_NO,    KC_NO,       MO(3),       KC_NO,   KC_NO,   KC_BSLASH, KC_NO,    KC_NO
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_0,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,    KC_MINUS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_EQUAL,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
   [_SYMBOLIC] = LAYOUT_ortho_4x10(
     KC_NO,     KC_NO,    KC_NO,    KC_NO,       KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
     KC_GRAVE,  KC_MINUS, KC_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_QUOTE,
-    KC_BSLASH, KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_QUOTE,
+    KC_BSLASH, KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_BSLASH,
     KC_NO,     KC_NO,    KC_NO,    KC_NO,       KC_NO,       MO(3),   KC_NO,   KC_NO,   KC_NO,    KC_NO
     ),
 
   [_FUNCTION] = LAYOUT_ortho_4x10(
-    QK_REBOOT, KC_NO,                KC_NO, KC_NO,                  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_F1,     KC_F2,                KC_F3, KC_F4,                  KC_F5,  KC_F6,  KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_F7,     KC_F8,                KC_F9, KC_F10,                 KC_F11, KC_F12, KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_NO,     MAGIC_SWAP_LCTL_LGUI, KC_NO, MAGIC_UNSWAP_LCTL_LGUI, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
   [_NAVIGATION] = LAYOUT_ortho_4x10(
-    QK_REBOOT, KC_NO,                KC_NO, KC_NO,                  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_F1,     KC_F2,                KC_F3, KC_F4,                  KC_F5,  KC_F6,  KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_F7,     KC_F8,                KC_F9, KC_F10,                 KC_F11, KC_F12, KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_NO,     MAGIC_SWAP_LCTL_LGUI, KC_NO, MAGIC_UNSWAP_LCTL_LGUI, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_UP  , KC_DOWN, KC_LEFT, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
   [_SYSTEM] = LAYOUT_ortho_4x10(
-    QK_REBOOT, KC_NO,                KC_NO, KC_NO,                  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_F1,     KC_F2,                KC_F3, KC_F4,                  KC_F5,  KC_F6,  KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_F7,     KC_F8,                KC_F9, KC_F10,                 KC_F11, KC_F12, KC_NO,   KC_NO,   KC_NO, KC_NO,
-    KC_NO,     MAGIC_SWAP_LCTL_LGUI, KC_NO, MAGIC_UNSWAP_LCTL_LGUI, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO, KC_NO
+    QK_REBOOT, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO,     KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO,     KC_NO, KC_NO, MAGIC_SWAP_LCTL_LGUI,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO,     KC_NO, KC_NO, MAGIC_UNSWAP_LCTL_LGUI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     )
 
 };
